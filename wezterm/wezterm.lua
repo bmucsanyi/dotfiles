27b7c3d2e55a3ebb41a3d8c3eaecdad09a027ac8
Bálint Mucsányi <50999947+bmucsanyi@users.noreply.github.com>
2025-03-03 22:34:02 +0100

    Add maximize on startup to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,15 @@
 local wezterm = require 'wezterm'
 local projects = require 'projects'
+local mux = wezterm.mux
 local config = wezterm.config_builder()
+
+-------------
+-- Startup --
+-------------
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
 
 ---------------------------
 -- Environment variables --
